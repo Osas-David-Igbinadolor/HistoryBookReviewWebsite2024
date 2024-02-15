bc490c3878c16c115790cfdf91155c273f3b12ac
Osas-David-Igbinadolor <98548190+Osas-David-Igbinadolor@users.noreply.github.com>
2024-02-15 19:29:03 +0000

    Create home.php
    Uploaded the first version of my Pages/home.php

---
+++ Pages/home.php
@@ -0,0 +1,22 @@
+    <div class="container">
+        <h1>Welcome to Our Book Review Site</h1>
+    </div>
+</header>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
