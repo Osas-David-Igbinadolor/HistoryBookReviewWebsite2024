5a928c305e3c06c37df4d792999bbd9cf3ffbf35
Osas-David-Igbinadolor <98548190+Osas-David-Igbinadolor@users.noreply.github.com>
2024-04-12 12:11:02 +0100

    This is the comment part of mywebsite

---
+++ CommentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Comment extends Controller
+{
+    public function store()
+    {
+        // Implement logic for storing comments
+    }
+}
